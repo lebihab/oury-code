208eee9a386dbc7230fd88058f7e8e860d758faf
lebihab <dmha89@gmail.com>
2023-01-08 08:50:49 +0000

    Oury-kindy and haw

--- oury-hello.py
+++ oury-hello.py
@@ -1,4 +1,4 @@
-print("Hello World")
+print("Hello Oury this is daddy  .. Hawlaotu is coming ")
 print("Hello World")
 print("I have a dream that my 4 little children will one day live in a nation where they will not be juddge at all")
 print("Hello Oury")
